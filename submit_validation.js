d0558adb0242a111d4550103180368fee1dfb5a7
LucasEPK <lucasmoyano11@outlook.it>
2023-07-06 19:38:35 -0300

    finished mobile version

--- submit_validation.js
+++ submit_validation.js
@@ -1,3 +1,15 @@
 function submit_validation(button_pressed) {
-    
+    // button pressed is document.querySelector('input[name="rating"]:checked');
+
+    event.preventDefault()
+
+    if (button_pressed != null) {
+        //code for when a rating was pressed
+        document.querySelector('.rating_card').style.display = "none";
+        document.querySelector('.thank_u_card').style.display = "flex";
+        document.querySelector('.orange_txt').innerHTML = "You selected "+button_pressed.value+" out of 5";
+    } else {
+        //code for when a rating wasn't pressed
+        alert('No rating pressed');
+    }
 }
